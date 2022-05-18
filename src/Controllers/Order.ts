8bb500159b17390a22982b8bab5cad80cadb784e
Arun Pillai <55800842+arunpillai6@users.noreply.github.com>
2022-05-18 13:18:10 +0000

    Update Order.ts
        const STRIPE_CLIENT_SECRET_KEY = '7CE556A3BC234CC1FF9E8A5C324C0BB70AA21B6D';
        const STRIPE_CLIENT_SECRET_KEY = '7CE556A3BC234CC1FF9E8A5C324C0BB70AA21B6D';

--- src/Controllers/Order.ts
+++ src/Controllers/Order.ts
@@ -55,7 +55,7 @@ export class Order {
 
   createStripeRequest(creditCard, price, address) {
     const STRIPE_CLIENT_ID = 'AKIA2E0A8F3B244C9986';
-    const STRIPE_CLIENT_SECRET_KEY = '7CE556A3BC234CC1FF9E8A5C324C0BB70AA21B6D';
+
     https.request(
       `http://invalidstripe.com?STRIPE_CLIENT_ID=${STRIPE_CLIENT_ID}&STRIPE_CLIENT_SECRET_KEY=${STRIPE_CLIENT_SECRET_KEY}&price=${price}&address=${JSON.stringify(
         address
